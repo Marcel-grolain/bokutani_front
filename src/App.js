d310ece81e44f27adab241b8f65458834cca0db6
Marcel-Grolain <mgrolain@gmail.com>
2024-02-20 05:57:32 +0100

    second commit, install react router dom

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Index from './components/dashboard/Index';
+
+function App() {
+  return (
+    <div className="App">
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
